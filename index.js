afcd642e22836b6ee9de0794753d2bcb27365cc5
Hisham6667 <hisham.44678@gmail.com>
2023-05-21 10:53:00 +0600

    add a toy to mongodb completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('The mystery maze shop server is running')
+  res.send('The mystery maze shop server is running')
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qd7bbha.mongodb.net/?retryWrites=true&w=majority`;
@@ -30,11 +30,25 @@ async function run() {
     await client.connect();
 
     const allToysCollection = client.db('mysteryMaze').collection('allToys')
+    const userToysCollection = client.db('mysteryMaze').collection('userToys')
 
-    app.get('/alltoys', async(req, res)=> {
-        const cursor = allToysCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
+    app.get('/alltoys', async (req, res) => {
+      const cursor = allToysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    app.get('/alltoys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await allToysCollection.findOne(query);
+      res.send(result);
+    })
+
+    // user toys
+    app.post('/usertoys', async(req, res) => {
+      const newToy = req.body;
+      const result = await userToysCollection.insertOne(newToy);
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
@@ -48,5 +62,5 @@ async function run() {
 run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`server is running in localhost:${port}`);
+  console.log(`server is running in localhost:${port}`);
 })
